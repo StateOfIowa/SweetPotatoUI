4a7abf4fddf345c2d0e6134b341c8be543492e56
Brent Millis <brent.millis@iowa.gov>
2017-09-22 16:01:00 -0500

    Updating AssemblyInfo.cs

--- SweetPotatoUI/Properties/AssemblyInfo.cs
+++ SweetPotatoUI/Properties/AssemblyInfo.cs
@@ -1,16 +1,15 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("UI_AutomationFramework")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("")]
+[assembly: AssemblyDescription("UI Automation Framework using SpecFlow and Selenium WebDriver")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("UI_AutomationFramework")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
